c89259330ae4b9d7c03e0a200f995ba5cd87dca8
Nicolas BRIET <fussure@gmail.com>
2018-12-08 16:08:39 +0100

    setup redux

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ class App extends Component {
     }
   }
 
+  componentDidMount(){
+    console.log(this.props)
+  }
+
   render() {
     return (
       <div className="App">
---
+++ src/reducers/RootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import gameReducer from './gameReducer';
+
+const RootReducer =  combineReducers({
+ gameReducer
+});
+
+export default RootReducer
