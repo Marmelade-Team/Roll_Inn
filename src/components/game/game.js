38ec6468ac2397d076ec9345adb3ecbca4b59722
Kamideux <matthieu.orriere@gmail.com>
2018-12-08 16:52:53 +0100

    test

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './style/game.css';
 
 class Game extends Component {
   
@@ -9,7 +10,11 @@ class Game extends Component {
 
   render() {
     return (
-      <h1>tests</h1>
+        <div className="container">
+            <div className="item item-1"></div>
+            <div className="item item-2"></div>
+            <div className="item item-3"></div>
+        </div>
     );
   }
 }
