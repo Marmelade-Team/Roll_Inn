05ce32fae3bb3314b0e235597c1a149c208871be
Denis <guiraudet@live.fr>
2018-12-10 11:02:09 +0100

    up utils

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import Home from './components/home/home'
 import GameContainer from './containers/gameContainer'
 import LobbiesContainer from './containers/lobbiesContainer'
